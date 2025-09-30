df1f4cf93e3b9f462b8e38b165aaec05b94c4958
Anirudh Parmar <anirudhparmar2004@gmail.com>
2025-09-30 22:52:55 +0530

    updated dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,7 +21,7 @@ const userId = session.user.id
  const isSubscribed = await isUserSubscribed()
  const isPurchased = await getUserPurchaseStatus()
 
-    if (!isSubscribed || isPurchased !== "paid") {
+    if (!isSubscribed && isPurchased !== "paid") {
         return ( <div className="absolute inset-0 z-10 rounded-lg  flex items-center justify-center">
         <div className="bg-white dark:bg-gray-900 p-8 rounded-lg shadow-lg text-center max-w-md">
           <h3 className="text-xl font-semibold mb-2">
